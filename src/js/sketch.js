df296ba7bc25e63136b19091c68683672130b2c5
antonevstigneev <anton@antonevstigneev.com>
2022-02-24 15:00:04 +0300

    added canvas size

--- src/js/sketch.js
+++ src/js/sketch.js
@@ -4,11 +4,11 @@ const sketch = (p5) => {
 
   const canvasWidth = p5.windowWidth;
   const canvasHeight = p5.windowHeight;
-
   window.p5 = p5;
 
   p5.setup = () => {
-    p5.background(0, 0, 0);
+    let canvas = p5.createCanvas(canvasWidth, canvasHeight);
+    p5.background(255, 255, 255);
   }
 
   p5.draw = () => {
